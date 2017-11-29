0197a545c71eb507260e65e087914eda4348ec96
Oucema Bellagha <bellagha.oucema@gmail.com>
2017-11-29 17:35:34 +0100

    list

--- python/S1/myprog.py
+++ python/S1/myprog.py
@@ -1,5 +1,7 @@
-age = input("Enter your age: ")
-new_age = float(age) + 50
-print(new_age)
+# age = input("Enter your age: ")
+# new_age = float(age) + 50
+# print(new_age)
 
 
+mylist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(mylist[18:])

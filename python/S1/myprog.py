8dc518d7d43c88f36b55241d758f205a6353deec
Oucema Bellagha <bellagha.oucema@gmail.com>
2017-12-22 16:54:38 +0100

    s2 + s3

--- python/S1/myprog.py
+++ python/S1/myprog.py
@@ -4,4 +4,16 @@
 
 
 mylist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-print(mylist[18:])
+# print(mylist[18:])
+
+a = 1
+b = "2"
+c = 3
+print(int(2.5))
+
+try:
+ z = c / 0
+except ZeroDivisionError:
+ z = 0
+
+print (z)

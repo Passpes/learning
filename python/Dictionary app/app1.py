031e6b11519f419fd1d750e4c27b88f70bab667a
Oucema Bellagha <bellagha.oucema@gmail.com>
2018-04-18 16:30:09 +0200

    debug

--- python/Dictionary app/app1.py
+++ python/Dictionary app/app1.py
@@ -2,9 +2,14 @@ import json
 from difflib import get_close_matches
 
 data = json.load(open("data.json"))
+i = 1
 
 def translate(w):
-    w = w.lower()
+    if w.istitle():
+        w = w
+    else:    
+        w = w.lower()
+        
     if w in data:
         return data[w]
     elif len (get_close_matches(w,data.keys())) > 0:

2bd5947599d9d8dea8c577a4a002e797975fa66a
Oucema Bellagha <bellagha.oucema@gmail.com>
2018-04-18 16:08:20 +0200

    commit app

---
+++ python/Dictionary app/app1.py
@@ -0,0 +1,27 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len (get_close_matches(w,data.keys())) > 0:
+        yn = input("We suggest the word %s instead, Enter Y if yes, or N if no: " % get_close_matches(w,data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        else:
+            exit()
+    else:
+        return "the word u are searching for doesn't exist!"
+
+word = input("Which word are u looking for: ")
+
+output = (translate(word))
+
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)
---
+++ python/S3/emailsloop.py
@@ -0,0 +1,9 @@
+# emails=['you@gmail.com','me@hotmail.com','they@hotmail.com']
+# for email in emails:
+#     if 'gmail' in email:
+#         print(email)
+
+mylist = [1,2,3,4,5]
+for i in mylist:
+    if i > 2:
+        print(i)
